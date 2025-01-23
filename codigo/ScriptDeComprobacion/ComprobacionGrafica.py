ef3bca51689db3f234061379f11e3e941f0c03dc
Raul-Gonzalez-M <raulgonmu@gmail.com>
2025-01-23 19:58:47 +0100

    modificacion

--- codigo/ScriptDeComprobacion/ComprobacionGrafica.py
+++ codigo/ScriptDeComprobacion/ComprobacionGrafica.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 
 # %%
 print("¿GPU detectada?:", tf.config.list_physical_devices('GPU'))
-strategy = tf.distribute.MirroredStrategy()
-print(f"Número de GPUs detectadas: {strategy.num_replicas_in_sync}")
+
 
 import torch
 print(torch.version.cuda)
