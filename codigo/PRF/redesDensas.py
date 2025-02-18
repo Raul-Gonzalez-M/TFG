c8daaff5b36f82b4f1f1494ea6412af63277eb1e
Raul-Gonzalez-M <raulgonmu@gmail.com>
2025-02-18 19:40:56 +0100

    redes LSTM .py

--- codigo/PRF/redesDensas.py
+++ codigo/PRF/redesDensas.py
@@ -104,22 +104,22 @@ def opti_redes_densas_multi_gpu(epoch_ini, epoch_fin, batch_array, numhoras, X_t
         for b in batch_array:
             best_value_of_the25 = 100
             best_model_of_the25 = None
-            #with tf.device('/CPU:0'):
-            for m in range(25): # Número de veces que se entrena cada modelo
-                with strategy.scope():
-                    model = Sequential()
-                    model.add(Dense(64, activation='relu', input_shape=(numhoras * 5,)))
-                    model.add(Dense(64, activation='relu'))
-                    model.add(Dense(1))
-                    model.compile(optimizer='adam', loss='mape')
+            with tf.device('/CPU:0'):
+                for m in range(25): # Número de veces que se entrena cada modelo
+                    with strategy.scope():
+                        model = Sequential()
+                        model.add(Dense(64, activation='relu', input_shape=(numhoras * 5,)))
+                        model.add(Dense(64, activation='relu'))
+                        model.add(Dense(1))
+                        model.compile(optimizer='adam', loss='mape')
 
-                    history = model.fit(X_train, y_train, epochs=e, batch_size=b, validation_data=(X_vali, y_vali), shuffle=False)
-                y_pred = model.predict(X_test)
-                valor = evalRedDensa(y_test, y_pred)
+                        history = model.fit(X_train, y_train, epochs=e, batch_size=b, validation_data=(X_vali, y_vali), shuffle=False)
+                    y_pred = model.predict(X_test)
+                    valor = evalRedDensa(y_test, y_pred)
 
-                if valor < best_value_of_the25:
-                    best_value_of_the25 = valor
-                    best_model_of_the25 = model
+                    if valor < best_value_of_the25:
+                        best_value_of_the25 = valor
+                        best_model_of_the25 = model
 
             print(f"Epoch: {e}, Batch size: {b}, Value: {best_value_of_the25}")
             
---
+++ codigo/PRLSTM/redesLSTM.py
@@ -0,0 +1,131 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+
+
+# %%
+df = pd.read_csv('SolAtasIMC_tratado.csv')
+df.head()
+
+# %%
+print(df.info())
+
+# %% [markdown]
+# # Preprocesado de Datos
+
+# %%
+tamanio = df.shape[0]
+
+# %%
+df_train = df.copy().loc[0:int(tamanio*0.7)]
+df_train
+
+# %%
+df_vali = df.copy().loc[int(tamanio*0.7 + 1):int(tamanio*0.9)]
+df_vali
+
+# %%
+df_test = df.copy().loc[int(tamanio*0.9 + 1):tamanio]
+df_test
+
+# %%
+df_valitest = pd.concat([df_vali, df_test], axis=0)
+
+# %% [markdown]
+# Numero de horas que se utilizan en la predicción
+
+# %%
+numhorasconst = 4
+
+# %% [markdown]
+# # Redes neuronales LSTM
+
+# %%
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import LSTM
+
+# %%
+def create_sequences(data, n_steps):
+    X, y = [], []
+    for i in range(len(data) - n_steps):
+        X.append(data[i:i+n_steps])
+        y.append(data[i+n_steps, 3])  
+    return np.array(X), np.array(y)
+
+# %%
+def preparar_datosLSTM(df, numhoras):
+    return create_sequences(df[['open', 'high', 'low', 'close', 'value']].values, numhoras)
+
+# %%
+def evalRedLSTM(ytest, y_pred):
+    y_pred = y_pred.flatten()
+    suma = 0
+    n = len(y_pred)
+    for i in range(0,n):
+        suma = abs(y_pred[i] - ytest[i])/ytest[i] +  suma
+    error_medio = suma/n
+    emp = error_medio*100 # error medio en porcentaje
+    return emp
+
+# %%
+def opti_redes_LSTM(epoch_ini, epoch_fin, batch_array, X_trainLSTM, y_trainLSTM, X_valiLSTM, y_valiLSTM, X_testLSTM, y_testLSTM, numhoras):
+    best = 100
+    epoch_best = 0
+    bacth_best = 0
+    best_model = None
+    for e in range(epoch_ini, epoch_fin + 1):
+        for b in batch_array:
+            for i in range(0, 25):
+                with tf.device('/CPU:0'):
+                    modelLSTM = Sequential()
+                    modelLSTM.add(LSTM(64, activation='relu', input_shape=(numhoras, 5)))
+                    modelLSTM.add(Dense(1))
+                    modelLSTM.compile(optimizer='adam', loss='mape')
+                    historyLSTM = modelLSTM.fit(X_trainLSTM, y_trainLSTM, epochs=e, batch_size=b, validation_data=(X_valiLSTM, y_valiLSTM), shuffle=False)
+                    y_pred = modelLSTM.predict(X_testLSTM)
+                    valor = evalRedLSTM(y_testLSTM, y_pred)
+                    print("epoch: "+str(e)+", batch_size: "+str(b)+", value: "+str(valor))
+                    if valor < best:
+                        best = valor
+                        epoch_best = e
+                        bacth_best = b
+                        best_model = modelLSTM
+                        cadena_guardado = "ModelosLSTMOptiMoreDataIMC/mi_modelo_LSTMOpti_e"+str(e)+"_b"+str(b)+"_v"+str(round(valor, 3))
+            best_model.save(cadena_guardado+".h5")
+            best_model.save(cadena_guardado+".keras")
+    return epoch_best, bacth_best, valor, best_model
+
+# %%
+BATCH_ARRAY = [4, 8, 12, 16, 20, 24, 28, 32, 40, 48, 64, 96, 128, 192, 256]
+
+# %%
+def opti_rLSTM_h(inih, finh, epoch_ini, epoch_fin, batch_array):
+    best = 100
+    epoch_best = 0
+    bacth_best = 0
+    h_best = 0
+    best_model = None
+    for i in range(inih, finh+1):
+        Xtrain, ytrain = preparar_datosLSTM(df_train, i)
+        Xvali, yvali = preparar_datosLSTM(df_vali, i)
+        Xtest, ytest = preparar_datosLSTM(df_test, i)
+        valores = opti_redes_LSTM(epoch_ini, epoch_fin, batch_array, Xtrain, ytrain, Xvali, yvali, Xtest, ytest, i)
+        if valores[2] < best:
+            best = valores[2]
+            epoch_best = valores[0]
+            bacth_best = valores[1]
+            h_best = i
+            best_model = valores[3]
+            cadena_guardado = "ModelosLSTMOptiMoreDataIMCBest/mi_modelo_LSTM_Opti_e"+str(epoch_best)+"_b"+str(bacth_best)+"_h"+str(i)+"_v"+str(round(best, 3))
+            best_model.save(cadena_guardado+".h5")
+            best_model.save(cadena_guardado+".keras")
+    return best, epoch_best, bacth_best, h_best, best_model
+
+# %%
+data = opti_rLSTM_h(7, 16, 3, 20, BATCH_ARRAY)
+
+
