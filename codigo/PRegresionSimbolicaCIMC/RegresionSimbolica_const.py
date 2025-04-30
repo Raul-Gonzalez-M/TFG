dd4ca0f1c1e2b2c980d64d8c4c483d9a90b02738
Raul-Gonzalez-M <raulgonmu@gmail.com>
2025-04-30 18:52:46 +0200

    425

--- codigo/PRegresionSimbolicaCIMC/RegresionSimbolica_const.py
+++ codigo/PRegresionSimbolicaCIMC/RegresionSimbolica_const.py
@@ -209,7 +209,7 @@ class RegresionSimbolica:
     
     def runcopy(self, numGenes, X, y, baremo: float, cargar:bool):
         resultado = []
-        num_veces = 400
+        num_veces = 425
         best = 1000000
         candidato_best = []
         self.genes = []
